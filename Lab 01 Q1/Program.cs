a0de341cbe8b588d134c396c0ef415bbc6a3355e
Imasha <Imasha@SILVERBUNNY>
2024-04-27 23:52:12 +0530

    Add project files.

---
+++ Lab 01 Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q1
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the rectangle:");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle:");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalculateArea(length, width);
+
+            Console.WriteLine($"The area of the rectangle is: {area}");
+        }
+
+        static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+
+
+}
+
+
+
+
+
